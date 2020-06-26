2f410c90ffb2628645456f087632a8f822470fd2
Domtary <2399783109@qq.com>
2020-06-26 14:17:46 +0800

    自增自减运算符

---
+++ test1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a=1;
+    int b=1;
+    
+    cout<<a++<<endl;
+    cout<<++b<<endl;
+    cout<<a<<endl;
+
+    system("pause");
+    return 0;
+}
